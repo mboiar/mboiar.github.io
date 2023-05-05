fb36be85b6c3943f0378cf475b369a27256e0658
Maksym Boiar <boiar.max@gmail.com>
2023-05-05 14:22:09 +0200

    add Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,26 @@
-export default function Navbar() {}
+import { ArrowRightIcon } from "@heroicons/react/solid";
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <header>
+            <div className="container row">
+                <a className="title" href="#about">
+                    Maksym Boiar
+                </a>
+                <nav>
+                    <a href="#projects">
+                        Projects
+                    </a>
+                    <a href="#skills">
+                        Skills
+                    </a>
+                </nav>
+                <a href="contact">
+                    Contact
+                    <ArrowRightIcon/>
+                </a>
+            </div>
+        </header>
+    )
+}
